5e04a552d7f89501cc29b696b85ebae9fc4ff550
atenohin <nohaylaajmi@gmail.com>
2023-06-06 00:19:29 +0200

    .

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdbool.h>
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: linked list to check
+ *
+ * Return: 0 if no cycle, 1 if cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *mainp = list;
+    listint_t *skipp = list;
+    bool exit_condition = true;
+
+    if (list == NULL)
+        return (0);
+
+    if (mainp == NULL || skipp == NULL)
+        exit = false;
+    else if (skipp->next == NULL)
+        exit = false;
+
+    while (exit)
+    {
+        mainp = mainp->next;
+        skipp = skipp->next->next;
+        if (mainp == skipp)
+            return (1);
+        if (mainp == NULL || skipp == NULL)
+            exit = false;
+        else if (skipp->next == NULL)
+            exit = false;
+    }
+    return (0);
+}
