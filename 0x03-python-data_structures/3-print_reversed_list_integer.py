08e2011d6bc6025c38b0e31881420386f5490f32
atenohin <nohaylaajmi@gmail.com>
2023-06-11 22:47:21 +0200

    Fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,4 @@ def print_reversed_list_integer(my_list=[]):
         print elements in reverse order
     """
     my_list_rev = [my_list[-i] for i in range(1, len(my_list) + 1)]
-    print(*my_list_rev, sep="\n")
+    print(*my_list_rev, sep="\n", end="")
