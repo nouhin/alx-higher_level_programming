754cbcaca2b9f97fe5da68532a18104a13750e63
atenohin <nohaylaajmi@gmail.com>
2023-06-11 17:00:00 +0200

    Add file

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """
+    Args:
+        my_list: a list
+    Return:
+        print elements in reverse order
+    """
+    my_list_rev = [my_list[-i] for i in range(1, len(my_list) + 1)]
+    print(*my_list_rev, sep="\n")
