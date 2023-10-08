47839225b5cb916ac2abe92e4b38114b447387a8
atenohin <nohaylaajmi@gmail.com>
2023-10-08 18:00:52 +0200

    .

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,8 +12,6 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
-        raise ValueError("size must be >=0")
-
+        raise ValueError("size must be >= 0")
     print(("#" * size + "\n") * size, end="")
