eec2d1aa9334276556af6e2da41b006a96b5c7c9
atenohin <nohaylaajmi@gmail.com>
2023-10-08 22:58:57 +0200

    Add file

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Prints ineteger"""
+
+
+def safe_print_integer(value):
+    """Functions that prints an integer
+
+    Args:
+        value (int): integer to print
+    """
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        print(f"Unexpecetd {err}")
