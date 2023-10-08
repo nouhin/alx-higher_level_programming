fe4baaab2c8853adc2262dc83d5d178d21b197f5
atenohin <nohaylaajmi@gmail.com>
2023-10-08 17:43:15 +0200

    Add file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that prints a square with the character # """
+
+
+def print_square(size):
+    """Function that prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >=0")
+    print(("#" * size + "\n") * size)
