80a5aca1e238ad489822410c5ebcfa1ef6dc9d73
atenohin <nohaylaajmi@gmail.com>
2023-10-09 22:54:55 +0200

    .

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,7 +8,7 @@ def safe_print_division(a, b):
     try:
         ans = a / b
     except Exception:
-        pass
+        ans = None
     finally:
-        print("{}".format(ans))
-    return ans
+        print("Inside result: {}".format(ans))
+        return ans
