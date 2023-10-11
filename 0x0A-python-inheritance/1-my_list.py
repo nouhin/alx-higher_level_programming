174c28748f933f25f08fef35befb73015edfa48b
atenohin <nohaylaajmi@gmail.com>
2023-10-11 23:23:03 +0200

    .

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defines a subclass of the built-in list class named MyList."""
 
-l
-class MyList(List)
-    """A subclass of the built-in list class."""
+
+class MyList(list):
+    """A subclass of the builtin list."""
+
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)."""
         print(sorted(self))
