ee55de1f56cfa148f1a9af3cf2e1fa43d4919c9d
atenohin <nohaylaajmi@gmail.com>
2023-10-11 12:48:29 +0200

    .

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """Represents a square"""
+    """Represents a suqare"""
 
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -11,3 +11,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size**2
