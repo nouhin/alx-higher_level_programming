07fbe091d2148d20f956556c04303856bb8cf53b
atenohin <nohaylaajmi@gmail.com>
2023-10-11 15:52:19 +0200

    .

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that append a given text to a given file"""
+
+
+def append_write(filename="", text=""):
+    """Appends a text to a file"""
+    if not isinstance(filename, str) or filename or filename == '':
+        raise ValueError("filename must a non empty string")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    with open(filename, 'b', encoding='utf-8') as f:
+        return f.write(text)
