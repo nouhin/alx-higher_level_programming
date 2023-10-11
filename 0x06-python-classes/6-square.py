2bbff65fb91fdafd00e73691cca3e819dc4a3084
atenohin <nohaylaajmi@gmail.com>
2023-10-11 14:50:02 +0200

    .

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,12 +6,8 @@ class Square:
     """Represents a suqare"""
 
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
