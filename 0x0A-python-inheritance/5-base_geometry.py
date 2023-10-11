77f849c72352300647538adb2ad7d642889b4b39
atenohin <nohaylaajmi@gmail.com>
2023-10-11 23:28:26 +0200

    .

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a placeholder class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a foundational geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Method not yet implemented."""
+        raise Exception("area() is not implemented")
