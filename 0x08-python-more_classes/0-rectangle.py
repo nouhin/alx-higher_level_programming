88803794e3bdcb5c05f15ba64d57e3f52cb01c4c
atenohin <nohaylaajmi@gmail.com>
2023-10-29 19:21:40 +0100

    .

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This script defines a Rectangle class."""
+
+
+class Rectangle:
+    """This class models a rectangle. It defines corresponding methods and attributes."""
+    pass
